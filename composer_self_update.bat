60c6ea630d770551aacdb998fd719ba07ead38d5
cptnk <kapitankapusta24@googlemail.com>
2013-07-23 23:13:19 +0200

    test commit

---
+++ composer_self_update.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+
+echo composer.phar self-update --------------------------------
+echo.
+php composer.phar self-update
+php composer.phar install
+
+pause
---
+++ composer_update.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+
+echo composer.phar update -------------------------------------
+echo.
+php composer.phar update
+
+pause
